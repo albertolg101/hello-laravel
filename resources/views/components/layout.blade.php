f7794faa94a8253836c602498165e643ee89a74e
Alberto Leyva Guerra (ALG) <albertoleyvaguerra@gmail.com>
2024-11-20 09:14:56 +0100

    feat: adds layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Slide & Pick</title>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        {{ $slot }}
+    </body>
+</html>

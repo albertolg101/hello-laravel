2a9f8fbd1c3b98e7ab9ad938038e46b9c5d10446
Alberto Leyva Guerra <albertoleyvaguerra@gmail.com>
2025-01-18 20:15:26 +0100

    fix: changes timezone from Spain/Madrid to Europe/Madrid

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('backup:clean')->dailyAt('3:10')->timezone('Spain/Madrid');
-Schedule::command('backup:run --only-db')->dailyAt('3:20')->timezone('Spain/Madrid');
-Schedule::command('backup:run')->sundays()->at('3:20')->timezone('Spain/Madrid');
+Schedule::command('backup:clean')->dailyAt('3:10')->timezone('Europe/Madrid');
+Schedule::command('backup:run --only-db')->dailyAt('3:20')->timezone('Europe/Madrid');
+Schedule::command('backup:run')->sundays()->at('3:20')->timezone('Europe/Madrid');

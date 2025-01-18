f5a52b68b5a8b14f526e2e8526a3221f8bcb6fed
Alberto Leyva Guerra <albertoleyvaguerra@gmail.com>
2025-01-18 20:02:12 +0100

    feat: schedules backups

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command('backup:clean')->dailyAt('3:10')->timezone('Spain/Madrid');
+Schedule::command('backup:run --only-db')->dailyAt('3:20')->timezone('Spain/Madrid');
+Schedule::command('backup:run')->sundays()->at('3:20')->timezone('Spain/Madrid');
